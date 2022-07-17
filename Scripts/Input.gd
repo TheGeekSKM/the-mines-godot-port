d64330eab7323cd60f8d937ab8e3faa93facd0ea
Sai Mangipudi <thegeekskm101@gmail.com>
2022-07-17 00:55:06 -0500

    Input and Fonts

---
+++ Scripts/Input.gd
@@ -0,0 +1,16 @@
+extends LineEdit
+
+
+
+func _ready() -> void:
+	grab_focus()
+
+
+
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
