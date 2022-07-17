f27cafaa89380c5c31f2ec13a616a8af8f10cec3
Sai Mangipudi <thegeekskm101@gmail.com>
2022-07-17 05:48:39 -0500

    More Room Edits

--- Scripts/Room.gd
+++ Scripts/Room.gd
@@ -3,3 +3,22 @@ class_name GameRoom
 
 export (String) var room_name = "Room Name"
 export (String) var rooom_description = "This is the description of the room."
+
+var exits: Dictionary = {}
+
+func connect_exit(direction: String, room: GameRoom):
+	match direction:
+		"north":
+			exits[direction] = room
+			room.exits["south"] = self
+		"south":
+			exits[direction] = room
+			room.exits["north"] = self
+		"east":
+			exits[direction] = room
+			room.exits["east"] = self
+		"west":
+			exits[direction] = room
+			room.exits["west"] = self
+		_:
+			printerr("Tried to connect to invalid direction: %s", direction)
